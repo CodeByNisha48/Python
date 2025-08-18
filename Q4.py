1da3767aa83ab9602fcb6e3705bb25c7f4962c6f
Nisha kumari <nishak96610@gmail.com>
2025-08-18 22:28:45 +0530

    Add files via upload

---
+++ Q4.py
@@ -0,0 +1,8 @@
+# print the elements of the following list using a loop.
+
+num = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+index = 0
+while (index <= len(num)):
+    print(num[index])
+    index += 1
+
