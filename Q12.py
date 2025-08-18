d745a0907d63413c2932692164bf028ab5cb7388
Nisha kumari <nishak96610@gmail.com>
2025-08-18 22:31:12 +0530

    Add files via upload

---
+++ Q12.py
@@ -0,0 +1,8 @@
+# WAP to find the factorial of first n numbers. (using for loop)
+
+n = int(input("Enter the number: "))
+fact = 1
+
+for i in range(1, n+1):
+    fact *= i
+print("Factorial:",fact)
