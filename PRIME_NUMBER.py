dacf198428af73b59a9d7d6765ea2af2660fa927
Nisha kumari <nishak96610@gmail.com>
2025-08-20 13:30:30 +0530

    Add files via upload

---
+++ PRIME_NUMBER.py
@@ -0,0 +1,14 @@
+n=int(input("Enter the number: "))
+
+def prime():
+    if n>1:
+        for i in range(2,n):
+            if n%i==0:
+                print(n,"is not prime")
+                break
+        else:
+            print(n,"is prime")
+    else:
+        print(n,"is prime")
+prime()
+
